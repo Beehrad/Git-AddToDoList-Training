293216f51cfbf7a83e5c8bee9715f3ace76e205a
Beehrad <beehrad@yahoo.com>
2023-02-25 15:41:52 +0330

    LocalStorage functions created

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const list = document.querySelector(".new")
 addBtn.addEventListener("click" , submit);
 clear.addEventListener("click" , löchen);
 list.addEventListener("click" , checkRemove)
+document.addEventListener("DOMContentLoaded", getLocalStorage)  
 
 
 function submit(e){
@@ -23,12 +24,14 @@ function submit(e){
     </div>`
     product.innerHTML = newProduct;
     list.appendChild(product);
+    saveLocalStorage(input.value);
     input.value = ""
     
 };
 
 function löchen(){
     list.innerHTML = ""
+    localStorage.clear();
     
 
 };
@@ -39,7 +42,46 @@ function checkRemove(e){
 
     if(classList[1] === "fa-trash"){
         const todo = item.parentElement.parentElement.parentElement;
+        removeLocalStorage(todo);
         todo.remove();
     }
 };
+
+
+
+function saveLocalStorage(todo){
+    let savedTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+    savedTodos.push(todo);
+    localStorage.setItem("todos" , JSON.stringify(savedTodos));
+}
+
+function getLocalStorage(){
+    let savedTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+    savedTodos.forEach(todo => {
+        const product = document.createElement("div");
+        product.classList.add("products");
+        const newProduct =`
+        <li>${todo}</li>
+        <div>
+        <span><i class="fa-solid fa-trash"></i></span>
+        </div>`
+        product.innerHTML = newProduct;
+        list.appendChild(product);
+    });
+}
+
+function removeLocalStorage(todo){
+    // console.log(todo.children[0].innerText])
+
+    let savedTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+
+    const filterdTodos = savedTodos.filter((t) => t !== todo.children[0].innerText);
+    localStorage.setItem("todos" , JSON.stringify(filterdTodos));
+}
 

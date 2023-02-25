dec75cfca36077c952ba030651bca5d0b3fe0230
Beehrad <beehrad@yahoo.com>
2023-02-25 15:37:42 +0330

    remove Button JS created

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const input = document.querySelector(".input_name")
 const addBtn = document.querySelector(".subbtn")
 const clear = document.querySelector(".clear")
-const trash = document.querySelector(".fa-trash")
 const list = document.querySelector(".new")
 
 
 
 addBtn.addEventListener("click" , submit);
 clear.addEventListener("click" , löchen);
+list.addEventListener("click" , checkRemove)
+
 
 function submit(e){
     e.preventDefault()
@@ -16,10 +17,10 @@ function submit(e){
     product.classList.add("products");
     const newProduct =`
     <li>${input.value}</li>
-    <span>
-    <i class="fa-solid fa-trash"></i>
-    </span>
-    `
+    <div>
+
+        <span><i class="fa-solid fa-trash"></i></span>
+    </div>`
     product.innerHTML = newProduct;
     list.appendChild(product);
     input.value = ""
@@ -28,5 +29,17 @@ function submit(e){
 
 function löchen(){
     list.innerHTML = ""
-}
+    
+
+};
+
+function checkRemove(e){
+    const classList = [...e.target.classList];
+    const item = e.target;
+
+    if(classList[1] === "fa-trash"){
+        const todo = item.parentElement.parentElement.parentElement;
+        todo.remove();
+    }
+};
 
